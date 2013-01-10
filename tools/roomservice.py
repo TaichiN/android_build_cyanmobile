abb26cfbda351c4c65693f276cbc1234f98c92a5
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-01-10 12:25:16 +0900

    roomservice: Add support fetch dependencies

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -46,10 +46,91 @@ for repository in repositories:
         f = open('.repo/local_manifest.xml', 'w')
         f.write(raw_xml)
         f.close()
-        
-        print "Syncing repository to retrieve project."
-        os.system('repo sync %s' % repo_path)
-        print "Done!"
-        sys.exit()
+
+def fetch_dependencies(repo_path):
+    print 'Looking for dependencies'
+    dependencies_path = repo_path + '/cm.dependencies'
+    syncable_repos = []
+
+    if os.path.exists(dependencies_path):
+        dependencies_file = open(dependencies_path, 'r')
+        dependencies = json.loads(dependencies_file.read())
+        fetch_list = []
+
+        for dependency in dependencies:
+            if not is_in_manifest("CyanogenMod/%s" % dependency['repository']):
+                fetch_list.append(dependency)
+                syncable_repos.append(dependency['target_path'])
+
+        dependencies_file.close()
+
+        if len(fetch_list) > 0:
+            print 'Adding dependencies to manifest'
+            add_to_manifest(fetch_list)
+    else:
+        print 'Dependencies file not found, bailing out.'
+
+    if len(syncable_repos) > 0:
+        print 'Syncing dependencies'
+        os.system('repo sync %s' % ' '.join(syncable_repos))
+
+def has_branch(branches, revision):
+    return revision in [branch['name'] for branch in branches]
+
+if depsonly:
+    repo_path = get_from_manifest(device)
+    if repo_path:
+        fetch_dependencies(repo_path)
+    else:
+        print "Trying dependencies-only mode on a non-existing device tree?"
+
+    sys.exit()
+
+else:
+    for repository in repositories:
+        repo_name = repository['name']
+        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+            print "Found repository: %s" % repository['name']
+            
+            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+            
+            default_revision = get_default_revision()
+            print "Default revision: %s" % default_revision
+            print "Checking branch info"
+            githubreq = urllib2.Request(repository['branches_url'].replace('{/branch}', ''))
+            add_auth(githubreq)
+            result = json.loads(urllib2.urlopen(githubreq).read())
+            
+            repo_path = "device/%s/%s" % (manufacturer, device)
+            adding = {'repository':repo_name,'target_path':repo_path}
+            
+            if not has_branch(result, default_revision):
+                found = False
+                if os.getenv('ROOMSERVICE_BRANCHES'):
+                    fallbacks = filter(bool, os.getenv('ROOMSERVICE_BRANCHES').split(' '))
+                    for fallback in fallbacks:
+                        if has_branch(result, fallback):
+                            print "Using fallback branch: %s" % fallback
+                            found = True
+                            adding['branch'] = fallback
+                            break
+                            
+                if not found:
+                    print "Default revision %s not found in %s. Bailing." % (default_revision, repo_name)
+                    print "Branches found:"
+                    for branch in [branch['name'] for branch in result]:
+                        print branch
+                    print "Use the ROOMSERVICE_BRANCHES environment variable to specify a list of fallback branches."
+                    sys.exit()
+
+            add_to_manifest([adding])
+
+            print "Syncing repository to retrieve project."
+            os.system('repo sync %s' % repo_path)
+            print "Repository synced!"
+
+            fetch_dependencies(repo_path)
+            print "Done"
+            sys.exit()
 
 print "Repository for %s not found in the CyanogenMod Github repository list. If this is in error, you may need to manually add it to your local_manifest.xml." % device
